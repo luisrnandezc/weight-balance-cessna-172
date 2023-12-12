d0a6a0b6f18e0699ca23a4087a0a69cbd658c135
Luis Hernandez <luisrnandezc@gmail.com>
2023-12-12 11:48:38 -0400

    Added Cessna172 and Cessna152 classes

---
+++ src/WeightAndBalance.java
@@ -0,0 +1,11 @@
+// TODO: add general project description.
+
+class Aircraft {
+    // TODO: intances declarations.
+}
+
+public class WeightAndBalance {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
