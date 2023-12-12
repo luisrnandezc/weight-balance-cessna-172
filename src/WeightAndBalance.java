b68fa1b96963fa5f0d8cf1e707dae117c1920031
Luis Hernandez <luisrnandezc@gmail.com>
2023-12-12 11:51:13 -0400

    Deleted aircraft.java file

--- src/WeightAndBalance.java
+++ src/WeightAndBalance.java
@@ -1,7 +1,15 @@
 // TODO: add general project description.
 
 class Aircraft {
-    // TODO: intances declarations.
+    // TODO: add instance variables and methods.
+}
+
+class Cessna172 extends Aircraft {
+    // TODO
+}
+
+class Cessna152 extends Aircraft {
+    // TODO
 }
 
 public class WeightAndBalance {
--- src/aircraft.java
+++
@@ -1,5 +0,0 @@
-public class aircraft {
-    public static void main(String[] args) {
-        // TODO
-    }
-}
