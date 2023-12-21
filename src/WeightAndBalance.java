fcd3d4f0784e0fe710d5dfb4bb9f0b7c19b59222
Luis Hernandez <luisrnandezc@gmail.com>
2023-12-20 22:47:07 -0400

    Updated code structure

--- src/WeightAndBalance.java
+++ src/WeightAndBalance.java
@@ -1,47 +1,52 @@
 // TODO: add general project description.
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 abstract class Aircraft {
 
+    public String aircraftModel;
     public String seatsConfig;
 
     abstract void setSeatsConfig(String config);
-    abstract int[] loadAircraft();
+    abstract int[] loadPayload();
 }
 
-class Cessna172 extends Aircraft {
+class Cessna extends Aircraft {
 
     public void setSeatsConfig(String config) {
         seatsConfig = config;
     }
 
-    public int[] loadAircraft() {
+    public int[] loadPayload() {
+        // TODO: Maybe a dictionary could work here.
         int[] onlyForTest = {1, 2, 3};
         return onlyForTest;
     }
 }
 
-class Cessna152 extends Aircraft {
+class DataReader {};
 
-    public void setSeatsConfig(String config) {
-        seatsConfig = config;
-    }
+class UserDataReader {};
 
-    public int[] loadAircraft() {
-        int[] onlyForTest = {1, 2, 3};
-        return onlyForTest;
-    }
-}
+class UserDataValidation {};
+
+class ResultsPlotter {};
 
 public class WeightAndBalance {
     public static void main(String[] args) {
+
         // TODO: read aircraft data for moments computation.
 
         // TODO: read user case data from text file.
         //      The text file should specify seating configuration,
         //      BEW information, fuel tanks capacity, total payload,
         //      and fuel consumed by starting, run-up and taxi.
+
+        // TODO: Checks validity of user case data.
+
+        // Initialization of the aircraft object.
+        Cessna aircraft = new Cessna();
 
         // TODO: load the the aircraft with the corresponding payload.
 
